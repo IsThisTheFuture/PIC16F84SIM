cd694d0d90a15290408f9843943cf1a0555aa7a5
IsThisTheFuture <mail@tobiaslamm.de>
2017-05-03 14:18:49 +0200

    Initial commit

---
+++ src/de/dhbw/Controller.java
@@ -0,0 +1,71 @@
+package de.dhbw;
+
+import javafx.event.ActionEvent;
+
+public class Controller {
+    public void openFile(ActionEvent actionEvent) {
+    }
+
+    public void openDocumentation(ActionEvent actionEvent) {
+    }
+
+    public void run(ActionEvent actionEvent) {
+    }
+
+    public void next(ActionEvent actionEvent) {
+    }
+
+    public void reset(ActionEvent actionEvent) {
+    }
+
+    public void clear(ActionEvent actionEvent) {
+    }
+
+    public void toggleA0(ActionEvent actionEvent) {
+    }
+
+    public void toggleA1(ActionEvent actionEvent) {
+    }
+
+    public void toggleA2(ActionEvent actionEvent) {
+    }
+
+    public void toggleA3(ActionEvent actionEvent) {
+    }
+
+    public void toggleA4(ActionEvent actionEvent) {
+    }
+
+    public void toggleA5(ActionEvent actionEvent) {
+    }
+
+    public void toggleA6(ActionEvent actionEvent) {
+    }
+
+    public void toggleA7(ActionEvent actionEvent) {
+    }
+
+    public void toggleB0(ActionEvent actionEvent) {
+    }
+
+    public void toggleB1(ActionEvent actionEvent) {
+    }
+
+    public void toggleB2(ActionEvent actionEvent) {
+    }
+
+    public void toggleB3(ActionEvent actionEvent) {
+    }
+
+    public void toggleB4(ActionEvent actionEvent) {
+    }
+
+    public void toggleB5(ActionEvent actionEvent) {
+    }
+
+    public void toggleB6(ActionEvent actionEvent) {
+    }
+
+    public void toggleB7(ActionEvent actionEvent) {
+    }
+}
