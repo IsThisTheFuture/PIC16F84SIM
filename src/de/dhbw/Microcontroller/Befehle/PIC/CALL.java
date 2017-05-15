6dad6a44d3675c3dd489d2ceff28c3e377da3e2c
IsThisTheFuture <mail@tobiaslamm.de>
2017-05-15 20:30:52 +0200

    CALL implementiert

--- src/de/dhbw/Microcontroller/Befehle/PIC/CALL.java
+++ src/de/dhbw/Microcontroller/Befehle/PIC/CALL.java
@@ -5,7 +5,7 @@ import de.dhbw.Microcontroller.Befehle.Instruction;
 
 /*
  * Call subroutine
- * TODO:  'PC + 1 -> TOS; k -> PC (Bits 0-10); PCLATH (Bits 3-4) -> PC (Bits 11-12)''0 -> f (Bit b)'
+ * TODO:  'PC + 1 -> TOS; k -> PC (Bits 0-10); PCLATH (Bits 3-4) -> PC (Bits 11-12)'
  */
 public class CALL extends Instruction {
     public CALL(int instruction, int opcode, int argument){
@@ -15,10 +15,32 @@ public class CALL extends Instruction {
     @Override
     public void execute(){
         int k = argument;
+        int pclath = memory.getAddress(Const.PCLATH);
 
-        stack.push(memory.getAddress(Const.PCL+1));
-        // TODO: PCL, PC, PCLATH benutzen?
-        memory.setAddress(Const.PCL, k);
+
+        // Top of Stack = PC + 1
+        stack.push(memory.getPc() +1);
+
+        // Die 11-Bit Addresse k wird in das 13-Bit Register geladen (Bit 0-10)
+        // Die oberen 2 Bits werden von PCLATH (Bit 3 und 4) geladen
+        pclath = pclath & 0b00011000;
+        // Damit sie die richtige Wertigkeit haben multiplizieren wir sie mit 256
+        /*
+            0b0000000011000 *1
+            0b0000000110000 *2
+            0b0000001100000 *4
+            0b0000011000000 *8
+            0b0000110000000 *16
+            0b0001100000000 *32
+            0b0011000000000 *64
+            0b0110000000000 *128
+            0b1100000000000 *256
+         */
+
+        // Jetzt addieren wir beide und schreiben sie in PC
+        memory.setPc(k+pclath);
+
+        // TODO: "CALL is 2-Cycle Instruction"
     }
 
     @Override
