5b9c3e24099df36be31bb113636e357bba1ad57a
IsThisTheFuture <mail@tobiaslamm.de>
2017-05-17 20:49:23 +0200

    Ignoriere Warning-Message beim Scrollen...

--- src/de/dhbw/Main.java
+++ src/de/dhbw/Main.java
@@ -6,10 +6,13 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import java.util.logging.LogManager;
+
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
+        LogManager.getLogManager().reset();
         Parent root = FXMLLoader.load(getClass().getResource("picsim.fxml"));
         primaryStage.setTitle("PIC16F84 Simulator");
         primaryStage.setScene(new Scene(root, 1000, 700));
